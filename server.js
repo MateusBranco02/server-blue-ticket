b05991fa0ecde90507ed1eded21710b8d1b8672f
Mateus Silva Branco <143047033+MateusBranco02@users.noreply.github.com>
2024-10-03 16:36:02 -0300

    instalação do cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const port = 3000;
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/', routes);
 
